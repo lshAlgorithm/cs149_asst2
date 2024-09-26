cf4ac90190d620304e6eb73bbe9810e3b1f30d6a
lshAlgorithm <lishuhuai_brian@163.com>
2024-09-27 00:04:53 +0800

    TOTALLY, ON MY OWN. Some correctness bug to be solved...

--- part_b/tasksys.cpp
+++ part_b/tasksys.cpp
@@ -121,6 +121,63 @@ void TaskSystemParallelThreadPoolSpinning::sync() {
  * Parallel Thread Pool Sleeping Task System Implementation
  * ================================================================
  */
+TaskState::TaskState():
+    done_(0) ,
+    num_total_task_(0),
+    task_distribute_(0)
+{
+    this->mutex_ = new std::mutex();
+}
+
+TaskState::TaskState(TaskID task_id, IRunnable* runnable, int num_total_task, const std::vector<TaskID>& deps):
+    task_id_(task_id),
+    num_total_task_(num_total_task),
+    runnable_(runnable),
+    done_(0),
+    task_distribute_(0)
+{
+    std::cerr << "create taskstate\t";
+    this->mutex_ = new std::mutex();
+    this->task_dep_ = deps;
+}
+
+TaskState::~TaskState() {
+    delete mutex_;
+}
+
+Subtask::Subtask(TaskState* task, int id) {
+    whole_task_ = task;
+    subtaskID_ = id;
+}
+
+Subtask::~Subtask() {}
+
+SubtaskBuffer::SubtaskBuffer() {
+    readyMutex_ = new std::mutex;
+    empty_ = new std::condition_variable;
+    emptyMutex_ = new std::mutex;
+}
+SubtaskBuffer::~SubtaskBuffer() {
+    delete readyMutex_;
+    delete empty_;
+    delete emptyMutex_;
+}
+
+TaskQueue::TaskQueue() {
+    std::cerr << "create taskqueue\t";
+    queMutex_ = new std::mutex;
+    que_ = new std::queue<TaskState>;
+    haveOne_ = new std::condition_variable;
+    // oneMutex_ = new std::mutex;
+}
+
+TaskQueue::~TaskQueue() {
+    std::cerr << "Delete taskqueue\t";
+    delete que_;
+    delete queMutex_;
+    delete haveOne_;
+    // delete oneMutex_;
+}
 
 const char* TaskSystemParallelThreadPoolSleeping::name() {
     return "Parallel + Thread Pool + Sleep";
@@ -133,6 +190,17 @@ TaskSystemParallelThreadPoolSleeping::TaskSystemParallelThreadPoolSleeping(int n
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+    threadPool_ = new std::thread[num_threads];
+    waiting_que_ = new TaskQueue();
+    ready_que_ = new SubtaskBuffer();
+    finish_set_ = new std::unordered_set<TaskID>;
+    nextMission_ = new std::condition_variable();
+
+    threadPool_[0] = std::thread(&TaskSystemParallelThreadPoolSleeping::Run, this);
+    for (int i = 1; i < num_threads; i++) {
+        // Use a class function in a thread
+        threadPool_[i] = std::thread(&TaskSystemParallelThreadPoolSleeping::sleepThread, this);
+    }
 }
 
 TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
@@ -142,10 +210,68 @@ TaskSystemParallelThreadPoolSleeping::~TaskSystemParallelThreadPoolSleeping() {
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+    
+    killed_ = true;
+    nextMission_->notify_all();
+    waiting_que_->haveOne_->notify_all();
+    for (int i = 0; i < num_threads_; i++) {
+        threadPool_[i].join();
+    }
+    delete finish_set_;
+    delete nextMission_;
+    delete []threadPool_;
+    delete ready_que_;
+    delete waiting_que_;
 }
 
-void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
+// Q: Any semaphore that can be utilized here?
+void TaskSystemParallelThreadPoolSleeping::sleepThread() {
+    // std::cerr << "sleep thread begin\t";
+    try {
+        while (true) {
+            if (killed_) 
+                break;
+            
+            // I encounter a bug here, saying operation not permitted. GPT says`在进行系统调用时，正确处理返回的错误码，并在必要时转换为异常抛出，而不是让错误码直接导致程序终止。`
+            // How to do it?
 
+            std::unique_lock<std::mutex> lk(*(ready_que_->readyMutex_));
+
+            if (ready_que_->buffer_.empty()) {
+                ready_que_->empty_->notify_all();
+                // Sleep...
+                nextMission_->wait(lk);
+            }
+
+            if (killed_) break;
+
+            Subtask &t = ready_que_->buffer_.front();
+            ready_que_->buffer_.pop();
+            lk.unlock();
+
+            t.whole_task_->mutex_->lock();
+            t.whole_task_->runnable_->runTask(t.subtaskID_, t.whole_task_->num_total_task_);
+            t.whole_task_->done_++;
+            if (t.whole_task_->done_ == t.whole_task_->num_total_task_) {
+                finish_set_->insert(t.whole_task_->task_id_);
+            }
+            t.whole_task_->mutex_->unlock();
+        }
+    } catch (const std::exception&e) {
+        std::cerr << "Exception!: " << e.what() << '\t';
+    }
+}
+
+// Override the serial run, to be compatible with the serial tests.
+void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_total_tasks) {
+    for (int i = 0; i < num_total_tasks; i++) {
+        runnable->runTask(i, num_total_tasks);
+    }
+}
+
+
+// Check if there is anything mission in *queue* can be pushed to the *buffer*
+void TaskSystemParallelThreadPoolSleeping::Run() {
 
     //
     // TODO: CS149 students will modify the implementation of this
@@ -153,8 +279,55 @@ void TaskSystemParallelThreadPoolSleeping::run(IRunnable* runnable, int num_tota
     // tasks sequentially on the calling thread.
     //
 
-    for (int i = 0; i < num_total_tasks; i++) {
-        runnable->runTask(i, num_total_tasks);
+    // initMutex_->lock();
+    bool fl;
+
+    while (true) {
+        if (killed_) 
+            break;
+        
+        TaskQueue &w = *waiting_que_;
+        std::unique_lock<std::mutex> lk(*(w.queMutex_));
+        if (w.que_->empty()) {
+            // sleep...
+            lk.unlock();
+            w.haveOne_->wait(lk);
+            if(killed_) {
+                lk.unlock();
+                break;
+            }
+        }
+        TaskState& newTask = w.que_->front();
+        w.que_->pop();
+        fl = true;
+        for (auto c: newTask.task_dep_) {
+            if (finish_set_->find(c) != finish_set_->end()) {
+                fl = false;
+                w.que_->push(newTask);
+            }
+        }
+        lk.unlock();
+        if (fl == false) {
+            continue;
+        }
+
+        /* 
+            There should only be one task_stat at all time...
+            No! Every task has its own task_stat_, bound with a taskID.
+        */
+
+        SubtaskBuffer &r = *ready_que_;
+        r.readyMutex_->lock();
+        for (int i = 0; i < newTask.num_total_task_; i++) {
+            Subtask subtask = Subtask(&newTask, i);
+            r.buffer_.emplace(subtask);
+        }
+        r.readyMutex_->unlock();
+        nextMission_->notify_all(); // wait and see...
+
+        /* We don't need it now. */
+        // task_stat_->idleMutex_->lock();  
+        // task_stat_->idleMutex_->unlock();
     }
 }
 
@@ -165,12 +338,25 @@ TaskID TaskSystemParallelThreadPoolSleeping::runAsyncWithDeps(IRunnable* runnabl
     //
     // TODO: CS149 students will implement this method in Part B.
     //
+    static TaskID id = 0;
 
-    for (int i = 0; i < num_total_tasks; i++) {
-        runnable->runTask(i, num_total_tasks);
+    waiting_que_->queMutex_->lock();
+
+    TaskID new_task_id = id++;
+
+    // You can make it a unique_ptr for better performance
+    TaskState new_task = TaskState(new_task_id, runnable, num_total_tasks, deps);
+    waiting_que_->que_->push(new_task);
+
+    // wake up Run()
+    if (waiting_que_->que_->size() == 1) {
+        waiting_que_->haveOne_->notify_all();
     }
 
-    return 0;
+    waiting_que_->queMutex_->unlock();
+
+
+    return new_task_id;
 }
 
 void TaskSystemParallelThreadPoolSleeping::sync() {
@@ -178,6 +364,15 @@ void TaskSystemParallelThreadPoolSleeping::sync() {
     //
     // TODO: CS149 students will modify the implementation of this method in Part B.
     //
+    
+    // Plan: sleep until the threads to complete
+    std::unique_lock<std::mutex> lk(*(ready_que_->emptyMutex_));
+    if (ready_que_->buffer_.empty()) 
+        return ;
+    else {
+        ready_que_->empty_->wait(lk);
+        lk.unlock();
+    }
 
     return;
 }
--- part_b/tasksys.h
+++ part_b/tasksys.h
@@ -3,6 +3,13 @@
 
 #include "itasksys.h"
 
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+#include <queue>
+#include <unordered_set>
+#include <iostream>
+
 /*
  * TaskSystemSerial: This class is the student's implementation of a
  * serial task execution engine.  See definition of ITaskSystem in
@@ -36,6 +43,7 @@ class TaskSystemParallelSpawn: public ITaskSystem {
         void sync();
 };
 
+
 /*
  * TaskSystemParallelThreadPoolSpinning: This class is the student's
  * implementation of a parallel task execution engine that uses a
@@ -54,6 +62,55 @@ class TaskSystemParallelThreadPoolSpinning: public ITaskSystem {
 };
 
 /*
+    Core data structure, recording all the details of a mission. 
+    Subtask just points to it.
+*/
+class TaskState {
+    public:
+        TaskState();
+        TaskState(TaskID task_id, IRunnable* runnable, int num_total_task, const std::vector<TaskID>& deps);
+        ~TaskState();
+        std::mutex* mutex_; // lock done_, task_distribute_, runnable, num_total_task_
+        TaskID task_id_;
+        int done_;
+        int num_total_task_;
+        int task_distribute_;
+        IRunnable* runnable_;
+
+        // new feature
+        std::vector<int> task_dep_;
+};
+
+class TaskQueue {
+    public:
+        TaskQueue();
+        ~TaskQueue();
+
+        std::mutex* queMutex_;
+        std::queue<TaskState>* que_;
+        std::condition_variable* haveOne_;
+};
+
+class Subtask {
+    public:
+        Subtask(TaskState* task, int id);
+        ~Subtask();
+
+        int subtaskID_;
+        TaskState* whole_task_;    // Directly point to the task itself, pay attention to mutex
+};
+
+class SubtaskBuffer {
+    public:
+        SubtaskBuffer();
+        ~SubtaskBuffer();
+
+        std::mutex* readyMutex_;
+        std::condition_variable* empty_;
+        std::mutex* emptyMutex_;
+        std::queue<Subtask> buffer_;
+};
+/*
  * TaskSystemParallelThreadPoolSleeping: This class is the student's
  * optimized implementation of a parallel task execution engine that uses
  * a thread pool. See definition of ITaskSystem in
@@ -68,6 +125,16 @@ class TaskSystemParallelThreadPoolSleeping: public ITaskSystem {
         TaskID runAsyncWithDeps(IRunnable* runnable, int num_total_tasks,
                                 const std::vector<TaskID>& deps);
         void sync();
+        void Run();
+        void sleepThread();
+    private:
+        bool killed_;
+        int num_threads_;
+        TaskQueue* waiting_que_;
+        SubtaskBuffer* ready_que_;
+        std::unordered_set<TaskID>* finish_set_;
+        std::thread* threadPool_;
+        std::condition_variable* nextMission_;
 };
 
 #endif
